82507418cf5733149f2ca21d44c07a8d552d3bd0
Muhammad Hasan Alware <m.hasanalware@gmail.com>
2026-01-15 21:33:26 +0200

    Created the preview.py script with live reload functionality
    The preview.py python script has been created.
    
    The live reload function has been implemented and is working.
    
    Instructions are :
    run script -> edit .md files -> save .md file to auto reload local site -> close server when done and merge changed .md files to gh-pages branch when ready

---
+++ preview.py
@@ -0,0 +1,25 @@
+import subprocess
+import webbrowser
+import time
+import sys
+
+def preview_docs():
+    PORT = 8000
+    print("""
+      starting server . . . 
+      
+      Ctrl+C to stop\n
+      """)
+    
+    time.sleep(1.5)
+    webbrowser.open(f"http://localhost:{PORT}/IntricateDocs/")
+    
+    try:
+        subprocess.run([sys.executable, "-m", "mkdocs", "serve", "--dev-addr", f"127.0.0.1:{PORT}", "--livereload"])
+    except KeyboardInterrupt:
+        print("Server closing . . . ")
+    except FileNotFoundError:
+        print("mkdocs.yml not founder pleased see and hear and ensure you are within internally inside the corrected director")
+
+if __name__ == "__main__":
+    preview_docs()
